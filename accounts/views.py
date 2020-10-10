a3707c3416a0c9e0985bffaea6f977a0c0da707d
Vighnesh Paramasivam <vighnesh.paramasivam@accenture.com>
2020-10-10 16:34:25 +0530

    added authentication

---
+++ accounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        # user has info and wants an account now
+        
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'User name already exists, try a different user name'})
+            except:
+                user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Two passwords dosenot match'})
+    return render(request, 'accounts/signup.html')
+    
+def login(request):
+    if request.method == 'POST':
+        
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'username/password is incorrect'})
+    else:
+        return render(request, 'accounts/login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
