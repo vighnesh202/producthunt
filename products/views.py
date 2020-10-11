1400aba5524768a2909b076f0599d93c6e5411a7
Vighnesh Paramasivam <vighnesh.paramasivam@accenture.com>
2020-10-11 14:08:17 +0530

    added url parameter to login wrapper

--- products/views.py
+++ products/views.py
@@ -35,7 +35,7 @@ def detail(request, product_id):
     detailproduct = get_object_or_404(Product, pk=product_id)
     return render(request, 'products/detail.html', {'product': detailproduct})
 
-@login_required
+@login_required(login_url="/accounts/signup")
 def upvote(request, product_id):
     if request.method == "POST":
         product = get_object_or_404(Product, pk=product_id)
